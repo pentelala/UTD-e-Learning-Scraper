2a0cdb3d0bb53026dc945eee7448cb4029b1a080
Abby <abbyaarce@gmail.com>
2024-06-14 18:37:46 -0500

    attempt at regex, doesn't work though sadly

--- scrapetest.py
+++ scrapetest.py
@@ -3,11 +3,13 @@ from bs4 import BeautifulSoup
 import re
 
 html = urlopen('https://en.wikipedia.org/wiki/Osamu_Dazai#Works')
-bs = BeautifulSoup(html, 'html.parser')
+bs = BeautifulSoup(html, 'lxml')
 
 def class_mod(tag):
     return tag.name=='td' and not tag.has_attr("rowspan")
 
 for child in bs.find('table', {'wikitable'}).find('tr').next_siblings:
+    onlyText = child.get_text()
     if child.find('td')!=-1:
+        textStrip = re.sub('<[^<]+?>', '', onlyText)
         print(child.find(class_mod))
